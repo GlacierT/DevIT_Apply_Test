dc1eadff7dddeeb3f3f4ef4bc95e300c1eabe649
T_Tanya <tikhonovskyat@gmail.com>
2020-10-15 21:23:16 +0300

    firs commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Main from "./component/main";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div>
+          <h1>Local gallery</h1>
+       </div>
+      </header>
+      <main>
+        <Main/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
