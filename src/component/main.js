f319a4c05d91bac42cd3c12b1c0e5153b229dfd0
T_Tanya <tikhonovskyat@gmail.com>
2020-10-21 17:32:10 +0300

    img info

--- src/component/main.js
+++ src/component/main.js
@@ -5,7 +5,7 @@ const Main = () => {
     
     const db = new Dexie("LocalGallery");
     db.version(1).stores({
-        posts: "title, content, file"
+        posts: "title, content, file, name, size, type, width, height, form"
     })
     db.open().catch((err) => {
         console.log(err.stack || err)
@@ -15,6 +15,12 @@ const Main = () => {
     const [postContent, setContent] = useState("");
     const [postFile, setFile] = useState("");
     const [posts, setPosts] = useState("");
+    const [postName, setName] = useState("");
+    const [postSize, setSize] = useState("");
+    const [postType, setType] = useState("");
+    const [postWidth, setWidth] = useState("");
+    const [postHeight, setHeight] = useState("");
+    const [postForm, setForm] = useState("");
 
     const getFile = (e) => {
         console.log(e)
@@ -24,6 +30,47 @@ const Main = () => {
         reader.onload= (e) => {
             setFile(reader.result);
         }
+
+        function readImageFile(cover) {
+            var reader = new FileReader(); 
+            reader.onload = function (e) {
+                var img = new Image();      
+                img.src = e.target.result;
+
+                img.onload = function () {
+                    var w = this.width;
+                    var h = this.height;
+                    if(w>h)
+                    {
+                        setForm('album');
+                    }
+                    else
+                    {
+                        if(h>w)
+                        {
+                            setForm('portrait');
+                        }
+                        else
+                        {
+                            setForm('square');
+                        }
+                    }
+                    setWidth(w);
+                    setHeight(h);
+                }
+            };
+            reader.readAsDataURL(cover);
+        }
+
+        var fileName, fileSize,fileType;
+        var fi = document.getElementById('cover');
+        fileName = fi.files.item(0).name;
+        fileSize = fi.files.item(0).size;
+        fileType = fi.files.item(0).type;
+        setName(fileName);
+        setSize(fileSize);
+        setType(fileType);
+        readImageFile(fi.files.item(0)); 
     }
   
     const deletePost = async(id) => {
@@ -39,7 +86,13 @@ const Main = () => {
             let post = {
                 title: postTitle,
                 content: postContent,
-                file: postFile
+                file: postFile,
+                name: postName,
+                size: postSize,
+                type: postType,
+                width: postWidth,
+                height: postHeight,
+                form: postForm
             }
            
     
@@ -63,24 +116,29 @@ const Main = () => {
   
     }, [])
 
+    let postData; 
 
-
-    let postData;
-  
-  
     if(posts.length > 0) {
       
         postData = <div className="postsContainer">
                     {
+                        
                         posts.map(post => {
                          
                              return <div className="post" key={post.title}>
                                         <div style={{backgroundImage: "url(" + post.file + ")" }} />
                                             <h2>{post.title}</h2>
                                             <p>{post.content}</p>
+                                            <h2>Image info</h2>
+                                            <p>Image name- {post.name}</p>
+                                            <p>Image size - {post.size} B</p>
+                                            <p>Image type - {post.type}</p>
+                                            <p>Image width - {post.width}</p>
+                                            <p>Image height - {post.height}</p>
+                                            <p>Image form - {post.form}</p>
                                             <a class="buttonHref" download href={post.file}>Download</a>
                                             <button className="delete" onClick={() => deletePost(post.title)}>Delete</button>
-                                        </div>   
+                                        </div>
                         })
                     }
                    </div>
